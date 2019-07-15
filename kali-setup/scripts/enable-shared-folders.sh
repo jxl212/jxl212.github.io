2e7ee164adc7e1dbe4cc3f023f2e08e2dda23e62
Jeremy LeCroy <jeremy212@gmail.com>
2019-07-14 21:35:53 -0400

    rebuilding site Sun Jul 14 21:35:53 EDT 2019

---
+++ kali-setup/scripts/enable-shared-folders.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+/usr/local/sbin/mount-shared-folders
+#!/bin/sh
+vmware-hgfsclient | while read folder; do
+  vmwpath="/mnt/hgfs/\${folder}"
+  echo "[i] Mounting \${folder}   (\${vmwpath})"
+  sudo mkdir -p "\${vmwpath}"
+  sudo umount -f "\${vmwpath}" 2>/dev/null
+  sudo vmhgfs-fuse -o allow_other -o auto_unmount ".host:/\${folder}" "\${vmwpath}"
+done
+sleep 2s
